78003a2d08378cbce2d19185795922e8ddf9012f
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-01-09 18:30:59 -0500

    Added Server.js and routes folder

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT= process.env.PORT || 3000;
+
+const app = express();
+
+//Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Serves Up public Directory
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { 
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+useCreateIndex: true });
+
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+  });
