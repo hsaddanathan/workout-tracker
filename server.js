8715b886e478df8ee930471539a80a0ecc4ef76c
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-01-09 19:02:12 -0500

    Added routing to the server file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const apiRoutes = require ("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
 
 const PORT= process.env.PORT || 3000;
 
@@ -13,6 +15,9 @@ app.use(express.json());
 
 //Serves Up public Directory
 app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
 useNewUrlParser: true,
